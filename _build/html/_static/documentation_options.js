a1a90314e58e7b6ec48ff87c07d5d1499b55187d
Yuki Fujiwara <sky.y.0079@gmail.com>
2019-12-19 18:18:58 +0900

    [update] sphinxビルド

--- _build/html/_static/documentation_options.js
+++ _build/html/_static/documentation_options.js
@@ -3,6 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '2019.02.21',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
