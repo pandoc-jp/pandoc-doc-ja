8c722f28a102414204ed2fe28df2e1eb9e3c2b38
Yuki Fujiwara <sky.y.0079@gmail.com>
2020-03-29 21:57:41 +0900

    [revise] ロゴ追加

--- conf.py
+++ conf.py
@@ -57,6 +57,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'alabaster'
 
 html_theme_options = {
+    'logo': 'icon_pandoc.jpg',
+    'logo_name': project,
     'github_user': 'pandoc-jp',
     'github_repo': 'pandoc-doc-ja',
 }
@@ -64,7 +66,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # フッタのソースコードへのリンク (Sphinxデフォルト) を消す
 html_show_sourcelink = False
